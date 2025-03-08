b283281270d2f8f638985388627c5625a9813b80
abdul-zabbar04 <a4zabbar@gmail.com>
2025-03-08 07:37:18 +0600

    category icon added

--- filterings/models.py
+++ filterings/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Category(models.Model):
     name= models.CharField(max_length=50)
     slug= models.SlugField(max_length=55)
+    icon= models.URLField(max_length=155, blank=True, null=True)
     def __str__(self):
         return self.name
 
